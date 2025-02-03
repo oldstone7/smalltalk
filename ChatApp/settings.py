2c7fe3a0a5f57fd74e67176ec131e51fd8d714f0
oldstone7 <sulaimanalfareeth@gmail.com>
2025-02-03 14:59:32 +0530

    DB Config

--- ChatApp/settings.py
+++ ChatApp/settings.py
@@ -112,10 +112,7 @@ DATABASES = {
         'USER': os.getenv("USER"),
         'PASSWORD': os.getenv("PASSWORD"),
         'HOST': os.getenv("HOST"),
-        'PORT':"5432",
-        'OPTIONS': {
-            'sslmode': 'require',  # NeonDB requires SSL mode
-        },
+        'PORT': os.getenv("PORT"),
         
     }
 }
